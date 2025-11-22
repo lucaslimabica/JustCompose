4f70cf5a9fa7462738105165f671bcab89535151
Lucas de Lima Bica <delimabica@gmail.com>
2025-11-22 00:19:18 +0000

    feat: Capture Now Captures Images
    now there are two models of capture, if the device is equals to 0, nothing changes, elif it's a str who ends with jpeg, png and etc, Camera will run imShow from OpenCV
    
    Other feature: now "q", "esc" and "l" close the JustCompose window

--- cam.py
+++ cam.py
@@ -10,28 +10,51 @@ class Camera:
     def __init__(self, name="Just Compose Beta", device=0):
         self.mp_hands = mp.solutions.hands 
         self.mp_drawing = mp.solutions.drawing_utils
-        
-        # Camera source TODO: change to a variable input
-        cap = cv.VideoCapture(device)
-        
-        with self.mp_hands.Hands() as hand_detector:
-            while cv.pollKey() == -1:
-                success, frame = cap.read()
-                if not success:
-                    print("Nothing to see here!!")
-                    continue
+        self.name = name
+        self.device = device
+        self.compatible_file_types = ('.jpg', '.jpeg', '.png')
+        self.capture()
+    
+    def capture(self):
+        if self.device == 0:        
+            cap = cv.VideoCapture(self.device)
+
+            with self.mp_hands.Hands() as hand_detector:
+                CAPTURE = cv.VideoCapture(self.device)
+                if not CAPTURE.isOpened(): 
+                    print("No video source :(")
+                    exit(1)
+                    
+                while CAPTURE.isOpened():
+                    ret, frame = CAPTURE.read()
+                    if not ret: # frame not captured
+                        print("The video has no frames")
+                        
+                    # Make detections
+                    results = hand_detector.process(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+                    if results.multi_hand_landmarks: # Avoid None for the drawing func
+                        # Call the drawing func
+                        self.draw_landmarks(frame, results)
+                    cv.imshow(self.name, frame)
+                    
+                    key = cv.waitKey(1)
+                    if key in [27, ord("q"), ord("l")]:
+                        break
                 
+        elif isinstance(self.device, str) and self.device.endswith(self.compatible_file_types):
+            image = cv.imread(self.device)
+
+            with self.mp_hands.Hands(static_image_mode=True) as hand_detector:
                 # Make detections
-                results = hand_detector.process(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+                results = hand_detector.process(cv.cvtColor(image, cv.COLOR_BGR2RGB))
                 if results.multi_hand_landmarks: # Avoid None for the drawing func
                     # Call the drawing func
-                    self.draw_landmarks(frame, results)
+                    self.draw_landmarks(image, results)
+
+                cv.imshow(self.name, image)
+                cv.waitKey(0)
+                cv.destroyAllWindows()
                 
-                cv.imshow(name, frame)
-            
-            cap.release()
-            cv.destroyAllWindows()
-            
     def draw_landmarks(self, image, results):
         for hand_landmarks in results.multi_hand_landmarks:
             self.mp_drawing.draw_landmarks(
@@ -45,3 +68,17 @@ class Camera:
             # 2º arg: landmarks to draw
             # 3º arg: the connections between the landmarks
             # 4º arg: style for the circles (landmarks)
+            # ==========================================================
+            
+            # print(hand_landmarks) ->
+            # landmark {
+            #   x: 0.389715612
+            #   y: 0.633941829
+            #   z: -0.0076536797
+            # }
+            # print(results.multi_hand_landmarks)
+            # [landmark {
+            #   x: 0.389715612
+            #   y: 0.633941829
+            #   z: -0.0076536797
+            # },...]
--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from cam import Camera
 
 def main():
     print("Hello from justcompose!")
-    Camera()
+    Camera(device=0) # device=r"C:\Users\lusca\Universidade\CV\TPs\TPFinal\JustCompose\assets\two_open_hands_example448x800.jpeg"
 
 
 if __name__ == "__main__":
