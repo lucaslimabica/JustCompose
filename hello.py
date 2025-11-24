53547a60d6221d97b4db90cb28adda6b18adf300
Lucas de Lima Bica <delimabica@gmail.com>
2025-11-24 22:27:54 +0000

    chore: main menu window
    Pygame Window Template from https://www.geeksforgeeks.org/python/how-to-create-buttons-in-a-game-using-pygame/
    
    It instances the Camera class as the button is pressed
    
    TODO: collapses the camera window whe the X button is clicked

--- hello.py
+++
@@ -1,9 +0,0 @@
-from cam import Camera
-
-def main():
-    print("Hello from justcompose!")
-    cam = Camera(device=0) # device=r"C:\Users\lusca\Universidade\CV\TPs\TPFinal\JustCompose\assets\two_open_hands_example448x800.jpeg"
-    cam.capture()
-
-if __name__ == "__main__":
-    main()
